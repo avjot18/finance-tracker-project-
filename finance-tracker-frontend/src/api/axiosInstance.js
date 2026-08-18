035977578992f2f1beaf1526c94b4c908216a5b8
AVJOT SINGH <121242086+avjot18@users.noreply.github.com>
2026-08-19 02:01:25 +0530

    Update axiosInstance.js

--- finance-tracker-frontend/src/api/axiosInstance.js
+++ finance-tracker-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: '/',
+  baseURL: 'https://finance-tracker-project-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },

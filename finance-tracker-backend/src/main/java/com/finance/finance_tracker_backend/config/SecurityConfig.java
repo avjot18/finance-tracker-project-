97e8ff228a4683d2ad2c0cabc7a89772388ab041
AVJOT SINGH <121242086+avjot18@users.noreply.github.com>
2026-08-19 09:51:36 +0530

    Update CORS configuration to include new origin
    Added new allowed origin for CORS configuration.

--- finance-tracker-backend/src/main/java/com/finance/finance_tracker_backend/config/SecurityConfig.java
+++ finance-tracker-backend/src/main/java/com/finance/finance_tracker_backend/config/SecurityConfig.java
@@ -76,6 +76,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList( "http://localhost:5173",
         "http://localhost:3000",
+                                                      "https://*.vercel.app"
         "https://finance-tracker-project-9m8fq2i8v-avjot-singhs-projects.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With", "Origin"));
